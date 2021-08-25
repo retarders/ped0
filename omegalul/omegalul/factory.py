bfaea895f6cdd4466079088612bfe54fc46ac481
pcranaway <pcranaway@tuta.io>
2021-08-26 01:33:09 +0300

    fix factory

--- omegalul/omegalul/factory.py
+++ omegalul/omegalul/factory.py
@@ -3,14 +3,17 @@ import random
 from typing import List
 from .chat import Chat
 from . import session
+from . import randid
+from . import api
 
 servers: List[str] = []
 
 def create_chat() -> Chat:
     sess = session.create_session()
 
+    global servers
     if not servers:
-        servers = json.loads(api.status(sess))['servers']
+        servers = json.loads(api.status(sess).text)['servers']
 
     server = random.choice(servers)
 
