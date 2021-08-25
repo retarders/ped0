0fa7f61d40797b7285c198dbf803aa34d5d5b272
pcranaway <pcranaway@tuta.io>
2021-08-26 01:26:53 +0300

    remove id module

--- omegalul/omegalul/id.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_randid():
-    chars = string.digits + string.ascii_uppercase
-    chars = chars.replace('I', '')
-    chars = chars.replace('O', '')
-
-    return ''.join(random.choice(chars) for i in range(8))
