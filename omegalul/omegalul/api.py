8ef34bd93cc01ad4336fd720de4d2333ef5b51af
pcranaway <pcranaway@tuta.io>
2021-08-26 00:27:56 +0300

    add typing and send endpoint methods

--- omegalul/omegalul/api.py
+++ omegalul/omegalul/api.py
@@ -5,3 +5,12 @@ def status(session: requests.Session):
 
 def events(session: requests.Session, server: str, id: str):
     return session.post('https://{}.omegle.com/events'.format(server), data={'id': id})
+
+def typing(session: requests.Session, server: str):
+    return session.post('https://{}.omegle.com/typing'.format(server), data={'id': id})
+
+def send(session: requests.Session, server: str, message: str):
+    return session.post('https://{}.omegle.com/send'.format(server), data={
+        'id': id,
+        'msg': message
+    })
