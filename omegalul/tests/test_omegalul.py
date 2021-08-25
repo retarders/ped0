7606975de4512da7b72526d99f4db5447959f12f
pcranaway <pcranaway@tuta.io>
2021-08-26 01:23:13 +0300

    add test

--- omegalul/tests/test_omegalul.py
+++ omegalul/tests/test_omegalul.py
@@ -1,5 +1,15 @@
 from omegalul import __version__
-
+import omegalul
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_start():
+    chat = omegalul.create_chat()
+
+    assert chat.id
+
+    try:
+        chat.stop()
+    except:
+        pass
