ac1242c57a29771028e17918f4f4c219f17ff9de
pcranaway <pcranaway@tuta.io>
2021-08-27 22:24:05 +0300

    add support for topics

--- omegalul/omegalul/api.py
+++ omegalul/omegalul/api.py
@@ -1,10 +1,17 @@
 import requests
+from typing import List
+import json
 
 def status(session: requests.Session):
     return session.get('https://omegle.com/status')
 
-def start(session: requests.Session, server: str, randid: str):
-    return session.post('https://{}.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&randid={}&lang=en'.format(server, randid))
+def start(session: requests.Session, server: str, randid: str, topics: List[str] = []):
+    url = 'https://{}.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&randid={}&lang=en'.format(server, randid)
+
+    if topics:
+        topics += '&topics=' + json.dumps(topics)
+
+    return session.post(url)
 
 def events(session: requests.Session, server: str, id: str):
     return session.post('https://{}.omegle.com/events'.format(server), data={'id': id})
